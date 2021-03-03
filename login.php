489cfb575188021375c20041744ad6fe37baaa09
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-03-03 14:55:19 +0700

    Update login.php

--- login.php
+++ login.php
@@ -62,14 +62,17 @@ session_start();
             if(isset($_POST['submit'])){
                 $username = $_POST["username"];
                 $password = $_POST["password"];
-                $query ="select role from accounts where username = '$username' and password = '$password' ";
-                $result = pg_query($pg_heroku,$query); 
-                $login_check = pg_fetch_assoc($result);
-                if($login_check == 'admin')
+                $query1 ="select username, password from accounts where username = '$username' and password = '$password' ";
+                $result1 = pg_query($pg_heroku,$query1); 
+                $login_check = pg_num_rows($result1);
+                $query2 ="select role from accounts where username = '$username' and password = '$password' ";
+                $result2 = pg_query($pg_heroku,$query2); 
+                $role_check = pg_fetch_assoc($result2);
+                if($login_check == 1 && $role_check == 'admin')
                 {        
                           header('location: storage.php');
                 }
-                elseif($login_check == 'staff')
+                elseif($login_check == 1 && $role_check == 'staff')
                 {
                           header('location: StorageManagement.php');
                 }
